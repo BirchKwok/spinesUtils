db059b8eb1a40526eec2835f88e4353d2c9517af
birchkwok <birchkwok@gmail.com>
2023-09-27 15:20:37 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spinesUtils',
-    version="0.3.0",
+    version="0.3.1",
     description='Machine-Learning Toolsets',
     keywords='machine learning',
     packages=find_packages(),
