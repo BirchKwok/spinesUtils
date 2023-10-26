f8c4eaa23d454bd5a2a371eda32dc3b2dfb55c87
birchkwok <birchkwok@gmail.com>
2023-10-26 11:34:38 +0800

    更新版本

--- spinesUtils/utils/_utils.py
+++ spinesUtils/utils/_utils.py
@@ -111,55 +111,56 @@ def reindex_iterable_object(obj, key=None, index_start=0):
 class Timer:
     """秒数为单位"""
     def __init__(self):
-        self.start_time = None
-        self.middle_points = []
+        self._start_time = None
+        self._middle_points = []
 
-    def check_is_start(self):
-        if self.start_time is None:
+    def check_is_started(self):
+        if self._start_time is None:
             raise RuntimeError("Timer is not started.")
 
     def start(self):
         """全局开始"""
-        self.start_time = time.time()
+        self._start_time = time.time()
         return self
 
     def middle_point(self):
         """中间点"""
-        self.check_is_start()
-        self.middle_points.append(time.time())
+        self.check_is_started()
+        self._middle_points.append(time.time())
         return self
 
     def end(self):
         """全局结束"""
-        self.check_is_start()
+        self.check_is_started()
 
-        time_diff = time.time() - self.start_time
+        time_diff = time.time() - self._start_time
 
-        self.start_time = None
-        self.middle_points = []
+        self._start_time = None
+        self._middle_points = []
         return time_diff
 
     def last_timestamp_diff(self):
         """上一点时间"""
-        self.check_is_start()
+        self.check_is_started()
 
-        if len(self.middle_points) == 0:
-            return time.time() - self.start_time
+        if len(self._middle_points) == 0:
+            return time.time() - self._start_time
 
-        time_diff = time.time() - self.middle_points[-1]
+        time_diff = time.time() - self._middle_points[-1]
         return time_diff
 
     def clear(self):
         """重置Timer"""
-        self.check_is_start()
+        self.check_is_started()
 
-        self.start_time = None
-        self.middle_points = []
+        self._start_time = None
+        self._middle_points = []
         return self
 
     def sleep(self, secs):
         """睡眠"""
-        self.check_is_start()
+        self.check_is_started()
 
         time.sleep(secs)
+        self._middle_points.append(time.time())
         return self
