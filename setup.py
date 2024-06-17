6eb96e69f7fd248cd400fd04699b97561131d331
birchkwok <birchkwok@gmail.com>
2024-06-17 11:54:02 +0800

    release 0.4.2 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spinesUtils',
-    version="0.4.1",
+    version="0.4.2",
     description='spinesUtils is a user-friendly toolkit for the machine learning ecosystem.',
     keywords='machine learning',
     packages=find_packages(),
